b799549c0cb5fb33b5acff710e7bc03150daaecc
George Ioannou <gmioannou@gmail.com>
2021-11-28 19:51:23 +0200

    initial commit

---
+++ demo_app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h2>Deploy with docker and nginx</h2>
+    <h3>Api: {{ apiUrl }}</h3>
+    <h3>Geoserver: {{ geoserverUrl }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      apiUrl: '',
+      geoserverUrl: ''
+    }
+  },
+  mounted() {
+    this.apiUrl = process.env.VUE_APP_API_URL
+    this.geoserverUrl = process.env.VUE_APP_GEOSERVER_URL
+  }
+
+}
+</script>
